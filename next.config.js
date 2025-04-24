c3d845866697fba1eca9ba1ce32af3be61785b55
usama397 <usamaashraf127@gmail.com>
2025-04-24 22:34:49 +0500

    fixed next export issue

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ["localhost"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-    ],
-  },
+  output: 'export', // ✅ THIS enables static export
 };
 
 module.exports = nextConfig;
+
