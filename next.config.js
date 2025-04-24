65d225b7d25a5ee833ab808213a095193f92b6ce
usama397 <usamaashraf127@gmail.com>
2025-04-25 04:37:35 +0500

    Fix: Rename next.config.cjs to next.config.js for Amplify detection

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+console.log('✅ next.config.cjs loaded');
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
+
