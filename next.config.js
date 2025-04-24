14be191333ac69d9fa71d2b67e67f6bf6e75e63f
usama397 <usamaashraf127@gmail.com>
2025-04-25 04:01:43 +0500

    Configure next export and Amplify for static deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  output: 'standalone',
+  output: 'export', // 🔥 This enables static export with real HTML files
+  trailingSlash: true, // optional: ensures `/page/` paths instead of `/page`
 };
 
 module.exports = nextConfig;
