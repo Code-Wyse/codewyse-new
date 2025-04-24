88a799e1c91d4ee4a030af0e1c6ff4cf9f194e46
usama397 <usamaashraf127@gmail.com>
2025-04-25 04:26:59 +0500

    Force re-track next.config.js for Amplify

--- next.config.js
+++
@@ -1,8 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // 🔥 This enables static export with real HTML files
-  trailingSlash: true, // optional: ensures `/page/` paths instead of `/page`
-};
-
-module.exports = nextConfig;
